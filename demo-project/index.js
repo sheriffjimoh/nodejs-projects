42c0a3807dcc1cadba5ebbddaf3a21dfc0713205
sheriffjimoh@gmail.com <sheriffjimoh@gmail.com>
2021-11-26 01:33:48 -0800

    initial

--- demo-project/index.js
+++ demo-project/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+// const express = require('express');
 
 // const app = express();
 
@@ -21,22 +21,33 @@ const express = require('express');
 //     res.send("<h1>Demo page Delete</h1>");
 // });
 
-const path = require('path')
+// const path = require('path')
 
-const app = express();
+// const app = express();
 
-app.use(function (req, res, next) {
-    console.log(req.method, req.path);
-    next();
-});
+// app.use(function (req, res, next) {
+//     console.log(req.method, req.path);
+//     next();
+// });
 
-app.use(express.static(path.join(__dirname, 'static')))
+// app.use(express.static(path.join(__dirname, 'static')))
 
-app.get('/demo', function (req, res) {    
-    res.sendFile(path.join(__dirname, 'static/index.html'))
-});
+// app.get('/demo', function (req, res) {    
+//     res.sendFile(path.join(__dirname, 'static/index.html'))
+// });
 
 
-app.listen(3000, function () {
-    console.log("Listening on port 3000. Go to http://localhost:3000");
-});
+// app.listen(3000, function () {
+//     console.log("Listening on port 3000. Go to http://localhost:3000");
+// });
+
+
+const os = require('os');
+
+console.log(os.userInfo());
+console.log("Platform: " + os.platform());
+console.log("Architecture: " + os.arch());
+console.log("Uptime: " + os.uptime());
+console.log("Os-name: " + os.type());
+console.log("Total memory: " + os.totalmem());
+console.log("free mem :")
